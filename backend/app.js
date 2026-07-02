83dcffa93e2a4fdfc30856794ce6b5adfb3c99b5
Rushikesh Auti <autirushikesh758@gmail.com>
2026-07-02 17:17:15 +0530

    Connect backend to database

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/database");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Connect Database
+connectDB();
 
 app.get("/", (req, res) => {
     res.json({
