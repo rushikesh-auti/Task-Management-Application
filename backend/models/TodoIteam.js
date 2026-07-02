6db600186ebae8584db51f14e04877942ecfa180
Rushikesh Auti <autirushikesh758@gmail.com>
2026-07-03 00:20:24 +0530

    Create TodoItem schema and model

---
+++ backend/models/TodoIteam.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const todoItemSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("TodoItem", todoItemSchema);
