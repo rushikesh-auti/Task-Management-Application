4ea2abd625285123669a1cc74bdd0db5e975626a
Rushikesh Auti <autirushikesh758@gmail.com>
2026-07-07 18:54:31 +0530

    Add get, update, and delete API routes

--- backend/routes/todoItemsRouter.js
+++ backend/routes/todoItemsRouter.js
@@ -3,6 +3,9 @@ const todoItemRouter = express.Router();
 
 const todoItemController = require("../controllers/todoItemsController");
 
+todoItemRouter.get("/", todoItemController.getTodoItem);
 todoItemRouter.post("/", todoItemController.createTodoItem);
+todoItemRouter.delete("/:id", todoItemController.deleteTodoItem);
+todoItemRouter.put("/:id/completed", todoItemController.markCompleted);
 
 module.exports = todoItemRouter;
